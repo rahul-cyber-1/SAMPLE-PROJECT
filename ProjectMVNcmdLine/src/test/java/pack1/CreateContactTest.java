68f5b535ae63e0fb6f79d0f03bb973fbd6caa933
git-cyber-1 <girija3770184@gmail.com>
2024-06-26 15:20:52 +0530

    framework added for jenkin

---
+++ ProjectMVNcmdLine/src/test/java/pack1/CreateContactTest.java
@@ -0,0 +1,26 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	
+	@Test
+	public void createContact() {
+		
+	    String URL=System.getProperty("url","loclahost:8888/");
+	    String BROWSER=System.getProperty("browser","chrome");
+	    String USERNAME=System.getProperty("username","admin");
+	    String PASSWORD=System.getProperty("password","admin");
+		System.out.println("contact is created");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+	}
+	
+	@Test
+	public void modifyContact() {
+		System.out.println(" contact modifed");
+	}
+
+}
---
+++ ProjectMVNcmdLine/src/test/java/pack2/CreateOrgTest.java
@@ -0,0 +1,17 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	
+	@Test
+	public void createOrg() {
+		System.out.println("Org is created");
+	}
+	
+	@Test
+	public void modifyOrg() {
+		System.out.println(" org modifed");
+	}
+
+}
